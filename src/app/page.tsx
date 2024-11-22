3b832bc116a9f2512d52bd2a9b00cd91470b995b
aymenae <aymenebrahim15@gmail.com>
2024-11-22 15:25:56 +0100

    Added min height to Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Homepage from "./components/Homepage";
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-br from-red-400 to-red-500 items-center p-2.5">
+    <div className="min-h-screen bg-gradient-to-br from-red-400 to-red-500 items-center p-2.5">
       <div className="bg-white m-1 p-4 rounded-xl ">
         <Navbar />
         <Hero />
