430cfd37f4db48dcbcc8ca032ca7fe9f7218218c
aymenae <aymenebrahim15@gmail.com>
2024-12-13 15:57:29 +0100

    Fix: updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
+/** @type {import('next').NextConfig} */
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   distDir: "build",
+  output: "export", // Enables static site export
 };
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 export default nextConfig;
